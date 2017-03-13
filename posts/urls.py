c6f50538a5edb22e5bbb991f01132cb7da3c9d1f
kanzeparov <kanzeparov@yandex.ru>
2017-03-13 20:24:07 +0300

    change comment

--- posts/urls.py
+++ posts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^blogs/$', BlogsList.as_view(), name="allblogs"),
     url(r'^core/$', test, name="mainpage"),
     url(r'^blogs/(?P<pk>\d+)/$', BlogView.as_view(), name="oneblog"),
-    url(r'^blogs/(?P<pk>\d+)/comments/$', CommentView.as_view(), name="allcomments"),
+    url(r'^blogs/(?P<pk>\d+)/comments/(?P<pk1>\d+)$', CommentView.as_view(), name="allcomments"),
   #  url(r'^comments/(?P<pk>\d+)/comments$', BlogView.as_view(), name="allcomment"),
 ]
